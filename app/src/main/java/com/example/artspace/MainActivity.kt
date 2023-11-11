3aa464fe8b7b5dfd4e674dcd1ce7de15c7814a2c
bib4maru <kirillosanov@yandex.ru>
2023-11-11 19:26:38 +0300

    Add layout of app and interactive buttons

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 
@@ -48,6 +64,85 @@ fun ArtSpaceApp(modifier: Modifier = Modifier){
     val list = ArrayList<Card>(4)
 
 
+}
+
+@Composable
+fun ArtCard(
+    modifier: Modifier = Modifier,
+    image: Int,
+    name: Int,
+    author: Int,
+    year: Int,
+    onNext: () -> Unit,
+    onPrevious: () -> Unit,
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(25.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Image(
+                    painterResource(image),
+                    null
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .background(Color(0xFFC9C8C5))
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+            ){
+                Text(text = stringResource(name), fontSize = 16.sp, fontWeight = FontWeight.Light)
+                Row() {
+                    Text(
+                        text = stringResource(author),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(text = "(${stringResource(year)})", fontSize = 14.sp)
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .padding(bottom = 20.dp)
+        ) {
+            Button(
+                onClick = onPrevious
+            ) {
+                Text(text = "Назад", color = Color.White)
+            }
+            Button(
+                onClick = onNext
+            ) {
+                Text(text = "Вперед", color = Color.White)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
